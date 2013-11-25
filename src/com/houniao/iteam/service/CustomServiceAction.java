e84b495d58c83cab396d5898e6197c1d1bc26255
bro_ <365492386@qq.com>
2013-11-25 09:19:54 +0800

    service

---
+++ src/com/houniao/iteam/service/CustomServiceAction.java
@@ -0,0 +1,10 @@
+package com.houniao.iteam.service;
+
+/**
+ * Created by Administrator on 13-11-22.
+ */
+public class CustomServiceAction {
+    static final public String ACTION_STARTED = "com.houniao.iteam.STARTED";
+    static final public String ACTION_UPDATE = "com.houniao.iteam.UPDATE";
+    static final public String ACTION_STOPPED = "com.houniao.iteam.STOPPED";
+}
